df02ec3e1518614562caaaa38cb16f83e1149bde
raidu4u <raidu.g6@gmail.com>
2018-12-10 15:36:44 +0530

    with password reset

--- colaberrylabs/settings.py
+++ colaberrylabs/settings.py
@@ -136,3 +136,10 @@ CRISPY_TEMPLATE_PACK = 'bootstrap'
 LOGIN_REDIRECT_URL= 'home'
 
 LOGIN_URL='login'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'raidu4u@gmail.com'
+EMAIL_HOST_PASSWORD = '8121653392'
